9e7d33b028d4ea52b85e0246bda9df92ae4372c3
Daniel Lennox <lennox.dan@gmail.com>
2017-10-25 16:17:38 +0200

    Switched to es6 syntax.

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 // @todo: Setup gulp, webpack and babel.
-// @todo: restart the app on file changes (watch).s
+// @todo: restart the app on file changes (watch).
 
+// Set a procress title so webpack can kill and restart the app on build.
+process.title = 'chatbot';
 
+//import express from 'express';
 const express = require('express');
-var builder = require('botbuilder');
+// Annoyingly, this needs to stay as require - https://github.com/Microsoft/BotBuilder/issues/2974
+const builder = require('botbuilder');
 
 // Setup Express Server.
-var app = express();
+let app = express();
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
 
 // Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ChatConnector({
+let connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
   appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
@@ -21,22 +25,21 @@ var connector = new builder.ChatConnector({
 app.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, [
- //session.send("You said: %s", session.message.text);
-  function (session) {
+let bot = new builder.UniversalBot(connector, [
+  (session) => {
     session.beginDialog('welcome', session.userData.greetings);
   },
-  function (session) {
+  (session) => {
     session.beginDialog('setup', session.userData.settings);
   }
 ]);
 
 bot.dialog('welcome', [
-  function (session) {
+  (session) => {
     builder.Prompts.confirm(session, 'Hey there! I\'m Gary, my name is just a placeholder I\'m not all that smart yet. My job is to make sure you complete your most important task everyday! Are you ready to get started?');
+    console.log('got here!!');
   },
-  function (session, results) {
-
+  (session, results) => {
     if (results.response) {
       session.endDialog();
     }
@@ -47,17 +50,17 @@ bot.dialog('welcome', [
 ]);
 
 bot.dialog('setup', [
-  function (session) {
+  (session) => {
     builder.Prompts.text(session, 'Awesome! What is your name?');
   },
-  function (session, results) {
+  (session, results) => {
     session.dialogData.name = results.response;
     builder.Prompts.time(session, `Hey ${session.dialogData.name}! What time would you like me to check in on you each day? eg, 6pm`);
     // @todo: Need validation to ensure it's just a time, not a full date.
     // @todo: NLP to check for o'clock, 6 (is it am or pm?) etc etc.
     // @todo: store time in userData ("userData stores information globally for the user across all conversations")
   },
-  function (session, results) {
+  (session, results) => {
     session.dialogData.checkinTime = builder.EntityRecognizer.resolveTime([results.response]);
     session.endDialog(`${session.dialogData.checkinTime} it is! You better have it done by then or I'm taking away your streak! And that's it for setup, I'll be checking in on you tomorrow buddy!`);
   }
@@ -71,10 +74,10 @@ bot.dialog('setup', [
 // bot.dialog('yesterDay', [
 
 bot.dialog('newDay', [
-  function (session) {
+  (session) => {
     builder.Prompts.text(session, 'Hi! What is the absolute most important thing you need to do today?');
   },
-  function (session, results) {
+  (session, results) => {
     session.endDialog(`OK! So you\'ve got until this time tomorrow to complete the following: \n ${results.response}!`);
   }
 ]);
