a1bf16f5e3ce025a6bffcad2e56a29bc4901f5e6
Daniel Lennox <lennox.dan@gmail.com>
2018-06-29 18:18:49 -0500

    Implemented countriesList model class.

---
+++ src/models/countriesList.model.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const { promisify } = require('util');
+const readFileAsync = promisify(fs.readFile);
+const _ = require('lodash');
+
+module.exports = class CountriesList {
+
+  constructor(countries){
+    this.countries = countries;
+  };
+
+  async loadAll() {
+    if (this.countries instanceof Array) {
+      return;
+    }
+    await readFileAsync('./src/data/countries.json', 'utf8')
+      .then((list) => {
+        this.countries = JSON.parse(list);
+      })
+      .catch((err) => {
+        console.log('ERROR:', err);
+      });
+  }
+
+  async getRandom(exclusions) {
+    await this.loadAll();
+
+    let countries = [];
+
+    if (exclusions) {
+      countries = _.filter(this.countries, (c) => {
+        return !_.find(exclusions, (e) => {
+          return e.Code === c.Code;
+        });
+      });
+    }
+    else {
+      countries = this.countries;
+    }
+
+    return countries[Math.floor(Math.random()*countries.length)];
+  }
+
+  async getList() {
+    await this.loadAll();
+    return this.countries;
+  }
+
+  async addCountry(country) {
+    await this.loadAll();
+    // @todo: Test for uniqueness and throw error if trying to add the same country twice.
+    return this.countries.push(country);
+  }
+
+  async removeRandom() {
+    await this.loadAll();
+    return this.countries.splice(_.random(this.countries.length - 1), 1)[0];
+  }
+};
