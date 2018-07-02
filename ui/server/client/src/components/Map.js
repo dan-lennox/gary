32a1db970cc67f6280ea1cfbfa4bba436987a5ce
Daniel Lennox <lennox.dan@gmail.com>
2018-07-02 13:01:43 -0500

    Added a hardcoded world map with countries.

--- ui/server/client/src/components/Map.js
+++ ui/server/client/src/components/Map.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Chart } from 'react-google-charts';
 
 class Map extends Component {
   renderContent() {
+
+    let options = {
+      defaultColor: '#FF0000',
+    };
+
+    let data = [['Country']];
+
+    data.push(['Germany']);
+    data.push(['France']);
+    data.push(['Russia']);
+    data.push(['Australia']);
+
     return (
-      <h1>Map</h1>
+      <div className="mapcontainer">
+        <Chart chartType="GeoChart"
+               width={"900px"}
+               height={"500px"}
+               data={data}
+               options={options}
+               graph_id="GeoChart"
+               mapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}/>
+      </div>
     );
   }
 
