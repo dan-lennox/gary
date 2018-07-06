6d07756c571165d9c2916b7c4e8e10cf04388700
Daniel Lennox <lennox.dan@gmail.com>
2018-07-06 16:33:46 -0500

    Added botTablePartition to Azure config.

--- src/config/azure-prod.js
+++ src/config/azure-prod.js
@@ -4,5 +4,6 @@ module.exports = {
   addressesTableName: 'todobotAddresses',
   // Define the Azure Table Partition. Azure Table Storage is designed for big data and so allows
   // table partitioning. Pretty sure we just need the one partition for this little app!
+  botTablePartition: 'part1',
   addressesTablePartition: 'part1'
 };
