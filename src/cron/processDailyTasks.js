9bfc6b9664c68269ef2fd91919f3a65385f22b74
Daniel Lennox <lennox.dan@gmail.com>
2018-08-07 16:07:17 -0500

    Wrapped 'processdailytasks' in db query to check user timezone and checkin time in preparation for resolving repeating messages issue.

--- src/cron/processDailyTasks.js
+++ src/cron/processDailyTasks.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const AzureConfig = require('../config/azure');
+const Azure = require('azure-storage');
 const addressModel = require('../models/address.model');
 
 module.exports = (bot) => {
@@ -10,8 +12,35 @@ module.exports = (bot) => {
     (addresses) => {
 
 
-      addresses.forEach((address) => {
-        bot.beginDialog(JSON.parse(address), "*:processDailyTasks");
+      addresses.forEach((addressJSON) => {
+
+        let address = JSON.parse(addressJSON);
+
+        console.log('Address to process', address);
+
+        // Connect to the Azure TableStorage.
+        let tableService = Azure.createTableService();
+
+        // Generate an Azure table query to search for Message Address table data for the given userId.
+        let query = new Azure.TableQuery()
+          .where('RowKey eq ?', 'userData')
+          .and('PartitionKey eq ?', address.user.id);
+
+          // Delete an address with the given User ID.
+          tableService.queryEntities(AzureConfig.botTableName, query, null, (error, result, response) => {
+            if (error) {
+              console.log('Error: ', error);
+            }
+            else {
+              // @todo: Move the storage of "timezone" into Settings.
+              console.log('checkinTime', response.body.value.settings.checkInTime);
+              console.log('timezone', response.body.value.timezone);
+
+              // @todo: Move the dialog logic in here to ensure only those user's who's checkin time has passed
+              // will actually have their conversations interrupted.
+              bot.beginDialog(address, "*:processDailyTasks");
+            }
+          });
       });
     },
     (error) => {}
